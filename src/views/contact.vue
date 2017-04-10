b3c40f6e9c6815195abeadac08843e6a7237f2cb
tutuzimu <1870260@163.com>
2017-04-10 21:41:02 +0800

    联系我们
    Signed-off-by: tutuzimu <1870260@163.com>

---
+++ src/views/contact.vue
@@ -0,0 +1,74 @@
+<template>
+    <!--网页头部-->
+    <Row justify="center" align="middle" class="header-bg">
+        <i-col span="3" offset="2">
+            您好，欢迎来到古韵婚纱店
+        </i-col>
+        <i-col span="11">
+            <i-button v-link="{path:'/login'}" type="text" class="header-login" id="color">hi，请登录</i-button>
+            <i-button v-link="{path:'/register'}" type="text" class="header-hover">免费注册</i-button>
+        </i-col>
+        <i-col span="8">
+            <i-input :value.sync="search" icon="ios-search" placeholder="请输入关键字" style="width: 200px"></i-input>
+            <i-button v-link="{path:'/personCenter'}" type="text" class="header-hover">个人中心</i-button>
+            <i-button v-link="{path:'/cart'}" type="text" class="header-hover">我的购物车</i-button>
+        </i-col>
+    </Row>
+
+    <!--首页nav-->
+    <div class="front-nav">
+        <Menu mode="horizontal" active-key="5">
+            <div class="front-nav-logo">
+                <img src="../images/guyun_logo_z.png" alt="logo" class="front-nav-logopic">&nbsp;&nbsp;&nbsp;&nbsp;
+                <img src="../images/guyun_logo.png" alt="logo" class="front-nav-logopic">
+            </div>
+            <div class="front-nav-item">
+                <Menu-item key="1">
+                    <Icon type="home"></Icon>
+                    首页
+                </Menu-item>
+                <Submenu key="2">
+                    <template slot="title">
+                        <Icon type="tshirt"></Icon>
+                        婚纱礼服
+                    </template>
+                    <Menu-item key="2-1">定制</Menu-item>
+                    <Menu-item key="2-2">购买</Menu-item>
+                    <Menu-item key="2-3">租赁</Menu-item>
+                </Submenu>
+                <Menu-item key="3">
+                    <Icon type="ios-copy"></Icon>
+                    设计投稿
+                </Menu-item>
+                <Menu-item key="4">
+                    <Icon type="ios-flame"></Icon>
+                    优惠活动
+                </Menu-item>
+                <Menu-item key="5" v-link="{path:'/contact'}">
+                    <Icon type="android-call"></Icon>
+                    联系我们
+                </Menu-item>
+            </div>
+        </Menu>
+    </div>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+        components: {},
+        data () {
+            return {}
+        },
+        methods: {},
+        ready () {
+
+        }
+    }
+</script>
