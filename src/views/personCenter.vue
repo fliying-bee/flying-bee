b584007c4da89eaaf1268c2415f5d30efc012884
tutuzimu <1870260@163.com>
2017-04-13 15:19:27 +0800

    个人中心
    Signed-off-by: tutuzimu <1870260@163.com>

--- src/views/personCenter.vue
+++ src/views/personCenter.vue
@@ -1,5 +1,20 @@
 <template>
-personCenter
+    <Row justify="center" align="middle" class="header-bg">
+        <i-col span="3" offset="2">
+            您好，欢迎来到古韵婚纱店
+        </i-col>
+        <i-col span="9">
+            <i-button v-link="{path:'/login'}" type="text" class="header-login" id="color">hi，请登录</i-button>
+            <i-button v-link="{path:'/register'}" type="text" class="header-hover">免费注册</i-button>
+        </i-col>
+        <i-col span="10">
+            <i-input :value.sync="search" icon="ios-search" placeholder="请输入关键字" style="width: 200px"></i-input>
+            <i-button v-link="{path:'/personCenter'}" type="text" class="header-hover">个人中心</i-button>
+            <i-button v-link="{path:'/cart'}" type="text" class="header-hover">我的购物车</i-button>
+            <i-button v-link="{path:'/index'}" type="text" id="color">返回首页</i-button>
+        </i-col>
+    </Row>
+
 </template>
 
 <style scoped>
