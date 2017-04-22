09e58546403685cd9a3f22ef8f1faa607a799406
tutuzimu <1870260@163.com>
2017-04-22 19:37:03 +0800

    个人中心

--- src/views/front/personCen/personInformation.vue
+++ src/views/front/personCen/personInformation.vue
@@ -54,7 +54,83 @@
                     </Breadcrumb>
                 </div>
                 <div class="back-content">
-                    <div class="back-content-main">内容区域</div>
+                    <div class="back-content-main">
+                        <i-col span="24">
+                            <Card class="preson-info">
+                                <div class="preson-info-div">
+                                    <div >
+                                        <Icon type="social-snapchat"></Icon>
+                                        <span class="person-Info-span">用户名</span>
+                                        <span></span>
+                                        <Icon type="edit"></Icon>
+                                    </div>
+                                    <div>
+                                        <Icon type="android-person"></Icon>
+                                        <span class="person-Info-span">性别</span>
+                                        <span></span>
+                                        <Icon type="female"></Icon>
+                                        <Icon type="male"></Icon>
+                                        <span></span>
+                                        <Icon type="edit"></Icon>
+                                    </div>
+                                    <div>
+                                        <Icon type="ios-locked"></Icon>
+                                        <span class="person-Info-span">密码</span>
+                                        <span></span>
+                                        <Icon type="edit"></Icon>
+                                    </div>
+                                    <div>
+                                        <Icon type="email"></Icon>
+                                        <span class="person-Info-span">邮箱</span>
+                                        <span></span>
+                                        <Icon type="edit"></Icon>
+                                    </div>
+                                    <div>
+                                        <Icon type="android-phone-portrait"></Icon>
+                                        <span class="person-Info-span">手机</span>
+                                        <span></span>
+                                        <Icon type="edit"></Icon>
+                                    </div>
+                                </div>
+                                <div>
+                                    <Icon type="ios-home"></Icon>
+                                    <span class="person-Info-span">地址</span>
+                                    <span class="person-Info-address"></span>
+                                    <div>
+                                        <i-form v-ref:form-dynamic :model="formDynamic" :label-width="80">
+                                            <Form-item
+                                                    v-for="item in formDynamic.items"
+                                                    :label="'地址' + ($index + 1)"
+                                                    :prop="'items.' + $index + '.value'"
+                                                    :rules="{required: true, message: '项目' + ($index + 1) +'不能为空', trigger: 'blur'}">
+                                                <Row>
+                                                    <i-col span="18">
+                                                        <i-input type="text" :value.sync="item.value" placeholder="请输入..."></i-input>
+                                                    </i-col>
+                                                    <i-col span="4" offset="1">
+                                                        <i-button type="ghost" @click="handleRemove(item)">删除</i-button>
+                                                    </i-col>
+                                                </Row>
+                                            </Form-item>
+                                            <Form-item>
+                                                <Row>
+                                                    <i-col span="12">
+                                                        <i-button type="dashed" long @click="handleAdd" icon="plus-round">新增</i-button>
+                                                    </i-col>
+                                                </Row>
+                                            </Form-item>
+                                            <Form-item>
+                                                <i-button type="primary" @click="handleSubmit('formDynamic')">提交</i-button>
+                                                <i-button type="ghost" @click="handleReset('formDynamic')" style="margin-left: 8px">重置</i-button>
+                                            </Form-item>
+                                        </i-form>
+                                    </div>
+                                    <Icon type="edit"></Icon>
+                                    <Icon type="ios-plus-outline"></Icon>
+                                </div>
+                            </Card>
+                        </i-col>
+                    </div>
                 </div>
                 <div class="back-copy">
                     2016-2017 &copy; 古韵婚纱
@@ -65,16 +141,58 @@
 </template>
 
 <style scoped>
-
+.preson-info{
+    padding: 20px;
+    font-size: 20px;
+}
+.preson-info-div{
+    line-height: 40px;
+}
+.person-Info-span{
+    width: 80px;
+    display: inline-block;
+}
+.person-Info-address{
+    display: inline-block;
+}
 </style>
 
 <script>
     export default {
         components: {},
         data () {
-            return {}
+            return {
+                formDynamic: {
+                    items: [
+                        {
+                            value: ''
+                        }
+                    ]
+                }
+            }
         },
-        methods: {},
+        methods: {
+            handleSubmit (name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('提交成功!');
+                    } else {
+                        this.$Message.error('表单验证失败!');
+                    }
+                })
+            },
+            handleReset (name) {
+                this.$refs[name].resetFields();
+            },
+            handleAdd () {
+                this.formDynamic.items.push({
+                    value: ''
+                });
+            },
+            handleRemove (item) {
+                this.formDynamic.items.$remove(item);
+            }
+        },
         ready () {
 
         }
