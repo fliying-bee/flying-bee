e1c1b9bb00212ebf78a94d0247568f74de97af58
tutuzimu <1870260@163.com>
2017-05-28 01:50:40 +0800

    订单明细

---
+++ src/views/back/detail/orderDetail.vue
@@ -0,0 +1,122 @@
+<template>
+    <!--网页头部-->
+    <div class="back-index-header clearfix">
+        <div class="back-header-logo">
+            <img src="/src/images/guyun_logo_z.png" alt="logo" class="back-header-logopic">&nbsp;&nbsp;&nbsp;&nbsp;
+            <img src="/src/images/guyun_logo.png" alt="logo" class="back-header-logopic">
+        </div>
+        <div class="back-header-nav">
+            <Dropdown v-if="isLogin">
+                <i-button type="text" class="header-hover">
+                    您好，{{empName}}
+                    <Icon type="arrow-down-b"></Icon>
+                </i-button>
+                <Dropdown-menu slot="list">
+                    <Dropdown-item v-link="{path:'/back/backPersonInfor'}">个人中心</Dropdown-item>
+                    <Dropdown-item @click="loginOut()">退出</Dropdown-item>
+                </Dropdown-menu>
+            </Dropdown>
+            <i-button v-else v-link="{path:'/back/backPersonInfor'}" type="text" class="header-hover">个人中心</i-button>
+        </div>
+    </div>
+
+    <div class="front-index-bg">
+        <Row type="flex">
+            <i-col span="5" class="back-menu-left">
+                <Menu active-key="1-1" width="auto" :open-keys="['1']">
+
+                    <Submenu key="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate"></Icon>
+                            客户管理
+                        </template>
+                        <Menu-item key="1-1" v-link="{path:'/back/orderMng'}">订单管理</Menu-item>
+                        <Menu-item key="1-2" v-link="{path:'/back/collectDraftMng'}">设计收稿</Menu-item>
+                        <Menu-item key="1-3" v-link="{path:'/back/inforMng'}">信息管理</Menu-item>
+                    </Submenu>
+                    <Submenu key="2">
+                        <template slot="title">
+                            <Icon type="ios-keypad"></Icon>
+                            运营管理
+                        </template>
+                        <Menu-item key="2-1" v-link="{path:'/back/commodityMng'}">商品管理</Menu-item>
+                        <Menu-item key="2-2" v-link="{path:'/back/purchaseMng'}">采购管理</Menu-item>
+                        <Menu-item key="2-3" v-link="{path:'/back/factoryMng'}">厂家管理</Menu-item>
+                        <Menu-item key="2-4" v-link="{path:'/back/empMng'}">员工管理</Menu-item>
+                        <Menu-item key="2-5" v-link="{path:'/back/formMng'}">报表管理</Menu-item>
+                        <Menu-item key="2-6" v-link="{path:'/back/powerMng'}">权限管理</Menu-item>
+                    </Submenu>
+                </Menu>
+            </i-col>
+            <i-col span="19">
+                <div class="back-breadcrumb">
+                    <Breadcrumb>
+                        <Breadcrumb-item href="#">后台管理</Breadcrumb-item>
+                        <Breadcrumb-item href="#">客户管理</Breadcrumb-item>
+                        <Breadcrumb-item href="#">订单管理</Breadcrumb-item>
+                        <Breadcrumb-item v-if="type=='buy'">购买单详情</Breadcrumb-item>
+                        <Breadcrumb-item v-if="type=='custom'">定制单详情</Breadcrumb-item>
+                        <Breadcrumb-item v-if="type=='rent'">租赁单详情</Breadcrumb-item>
+                    </Breadcrumb>
+                </div>
+                <div class="back-content">
+                    <div class="back-content-main">
+
+                    </div>
+                </div>
+                <div class="back-copy">
+                    2016-2017 &copy; 古韵婚纱
+                </div>
+            </i-col>
+        </Row>
+        <!--<Spin fix v-if="isLoading">-->
+            <!--<Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>-->
+            <!--<div>Loading</div>-->
+        <!--</Spin>-->
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+
+        components: {},
+        data () {
+            return {
+                empName:'',
+                isLogin:false,
+                isLoading:true,
+                orderId:this.$route.params.orderId,
+                type:this.$route.params.type,
+
+            }
+        },
+        methods: {
+            loginOut(){
+                var self = this;
+                localStorage.removeItem('EMPNAME');
+                localStorage.removeItem('EMPID');
+                self.$Message.success('退出成功！');
+                setTimeout(()=>{
+                    self.$router.go('/login');
+                    self.isLogin = false;
+                },1000);
+            }
+        },
+        ready () {
+            var self = this;
+            if(localStorage.getItem('EMPNAME')){
+                self.empName = localStorage.getItem('EMPNAME');
+                self.isLogin = true;
+            }else{
+                self.isLogin = false;
+            }
+    }
+
+
+
+    }
+</script>
